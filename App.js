1ae606e9a3ed25d51a94a073f17881a37fe7e903
andrewflbarnes <andrew.barnes2@mastercard.com>
2019-04-16 00:14:35 +0100

    Add react navigation and basic homescreen
    - Dummy routes added

--- App.js
+++ App.js
@@ -8,6 +8,8 @@
 
 import React from 'react';
 import Races from './components/Races';
+import HomeScreen from './components/HomeScreen';
+import Hello from './components/Hello';
 
 // const instructions = Platform.select({
 //   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -15,9 +17,15 @@ import Races from './components/Races';
 //     'Double tap R on your keyboard to reload,\n' +
 //     'Shake or press menu button for dev menu',
 // });
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 
-export default function App() {
-  return (
-    <Races />
-  )
-}
+const MainNavigator = createStackNavigator({
+  Navigation: {screen: HomeScreen},
+  Seeding: {screen: Hello},
+  Management: {screen: Hello},
+  Round1: {screen: Races},
+  Round2: {screen: Races},
+  Knockouts: {screen: Races},
+});
+
+export default App = createAppContainer(MainNavigator);
